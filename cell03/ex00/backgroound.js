e387b3125b2ca9bbe8b6d85ce69ef1599ea4a388
Waroon Ragwongsiri <wragwong@z2t17c4.42bangkok.com>
2025-04-29 18:15:39 +0700

    cell03ex00fix

--- cell03/ex00/backgroound.js
+++ cell03/ex00/backgroound.js
@@ -1,4 +1,14 @@
+let button = document.getElementById("colorButton")
+
+button.style.width = "200px"
+button.style.fontSize = "20px"
+
+button.addEventListener("click",()=>{
+    button.style.width = `${parseInt(button.style.width.replace("px",""))+20}px`;
+    button.style.fontSize = `${parseInt(button.style.fontSize.replace("px",""))+2}px`;
+})
+
 function randomColor(){
     let hexColor = Math.floor(Math.random() * 256*256*256).toString(16)
-    document.body.style.backgroundColor = "#"+hexColor
+    document.body.style.backgroundColor = "#"+hexColor    
 }
