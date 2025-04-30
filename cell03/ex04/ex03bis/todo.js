fc9dfea99e2c6220e97aadbbb759bef4e427c79a
Waroon Ragwongsiri <wragwong@z2t17c4.42bangkok.com>
2025-04-30 14:22:59 +0700

    cell03 ex04 use prettier

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,12 +1,12 @@
 function setCookie(name, value, daysToLive) {
-    const date = new Date();
-    date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
-    let expires = "expires=" + date.toUTCString();
-    document.cookie = `${name}=${value}; ${expires}; path=/`;
-  }
+  const date = new Date();
+  date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
 
 function deleteCookie(name) {
-  setCookie(name, null,null);
+  setCookie(name, null, null);
 }
 
 function getAllCookie() {
@@ -16,57 +16,62 @@ function getAllCookie() {
   return cArray;
 }
 
-function readCookieToDiv(){
-    const ft_list = $("#ft_list");
-    if(todolist[0] == ''){
-        return
-    }
-    for(let i=0;i<todolist.length;i++){
-        let todo_name = todolist[i].split("=")[0]
-        let todo_val = todolist[i].split("=")[1]
-        ft_list.append(`<div class="todo" id="${todo_name}"><p>${todo_val}</p></div>`);
-    }
+function readCookieToDiv() {
+  const ft_list = $("#ft_list");
+  if (todolist[0] == "") {
+    return;
+  }
+  for (let i = 0; i < todolist.length; i++) {
+    let todo_name = todolist[i].split("=")[0];
+    let todo_val = todolist[i].split("=")[1];
+    ft_list.append(
+      `<div class="todo" id="${todo_name}"><p>${todo_val}</p></div>`
+    );
+  }
 }
 
-function popUpCreateTodo(){
-    let text = prompt("Enter your Todo")
-    if(text == null || text == ''){
-        return
-    }
-    if(todolist[0] == ""){
-        setCookie(0,text,365)
-        location.reload()
-        return
-    }
-    let last_name = todolist[todolist.length - 1].substring(0,todolist[todolist.length - 1].indexOf("="))
-    setCookie(parseInt(last_name) + 1,text,365)
-    location.reload()
-    return
+function popUpCreateTodo() {
+  let text = prompt("Enter your Todo");
+  if (text == null || text == "") {
+    return;
+  }
+  if (todolist[0] == "") {
+    setCookie(0, text, 365);
+    location.reload();
+    return;
+  }
+  let last_name = todolist[todolist.length - 1].substring(
+    0,
+    todolist[todolist.length - 1].indexOf("=")
+  );
+  setCookie(parseInt(last_name) + 1, text, 365);
+  location.reload();
+  return;
 }
 
-function confirmDelete(name){
-    if(confirm(`delete?`)){
-        deleteCookie(name)
-    }
-    location.reload()
+function confirmDelete(name) {
+  if (confirm(`delete?`)) {
+    deleteCookie(name);
+  }
+  location.reload();
 }
 
 //declare variable
-const btn = $("#new")
-let todolist = getAllCookie()
+const btn = $("#new");
+let todolist = getAllCookie();
 
 //read cookie to div
-readCookieToDiv()
+readCookieToDiv();
 
-console.log(todolist)
+console.log(todolist);
 
-btn.on('click',()=>{
-    popUpCreateTodo()
-})
+btn.on("click", () => {
+  popUpCreateTodo();
+});
 
-const todos = document.getElementsByClassName("todo")
-for(let i=0;i<todolist.length;i++){
-  todos[i].addEventListener("click",()=>{
-    confirmDelete(todolist[i].split('=')[0])
-  })
+const todos = document.getElementsByClassName("todo");
+for (let i = 0; i < todolist.length; i++) {
+  todos[i].addEventListener("click", () => {
+    confirmDelete(todolist[i].split("=")[0]);
+  });
 }
